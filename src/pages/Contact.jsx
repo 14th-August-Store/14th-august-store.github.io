a5b9d8ae422f325af06c2ee48e59255c131696bb
14th-August-Store <rizwanchandna6.@gmail.com>
2026-08-06 19:13:52 +0500

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -72,7 +72,7 @@ function Contact() {
   <div className="contact-map">
     <iframe
       title="Pakistan Store Location"
-      src="https://www.google.com/maps?q=Karachi,Pakistan&output=embed"
+      src="https://www.google.com/maps?q=Shop+12,+Al-Hamra+Avenue,+Opp.+Bara+Market,+Sector+5K,+North+Karachi,+Karachi,+Pakistan&output=embed"
       width="100%"
       height="450"
       style={{ border: 0 }}
