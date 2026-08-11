3ea6707edfad95f1cdd9752d8be0476c6e52f752
14th-August-Store <rizwanchandna6.@gmail.com>
2026-08-11 20:07:43 +0500

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -33,7 +33,7 @@ import badge3 from "../images/badges/curlybadge.jpg";
 import badge4 from "../images/badges/badge.jpg";
 import badge5 from "../images/badges/rubberbadge.jpg";
 import badge6 from "../images/badges/pakistanbadge.jpg";
-import badge7 from "../images/badges/ledbadge.jpg";
+import badge7 from "../images/badges/ledbadge.png";
 
 import mask1 from "../images/masks/14augustmask.jpg";
 import mask2 from "../images/masks/azontomask.jpg";
